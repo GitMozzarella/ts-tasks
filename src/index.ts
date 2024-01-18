2a55bc5573e2e3c7b328d0e93f524efaecbee5e4
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-01-19 02:57:11 +0600

    task 1 done

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,30 @@
-const totalPrice = ({ price, discount, isInstallment, months }) => {
-	// Your code here...
+type totalPriceProps = {
+	price: number
+	isInstallment: boolean
+	months: number
+	discount?: number
+}
+
+const totalPrice = ({
+	price,
+	isInstallment,
+	months,
+	discount
+}: totalPriceProps) => {
+	if (isInstallment) {
+		if (discount) {
+			let priceOfDiscount = (price / 100) * discount
+			let resultSum = (price - priceOfDiscount) / months
+			return resultSum
+		} else {
+			return price / months
+		}
+	} else {
+		if (discount) {
+			return price - discount
+		}
+	}
+	return price
 }
 
 const price = totalPrice({
@@ -8,4 +33,12 @@ const price = totalPrice({
 	isInstallment: true,
 	months: 12
 })
-console.log(price) // 6250
+
+const price2 = totalPrice({
+	price: 60000,
+	isInstallment: true,
+	months: 12
+})
+
+console.log(price)
+console.log(price2)
