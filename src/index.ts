3371c3f24e46ce775cf9f452cbe2e3e08d652e10
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-01-21 23:12:01 +0600

    task2 done

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
-const posts = [
+interface IProps {
+	id: string
+	title: string
+	body: string
+}
+interface NormalizedData {
+	byId: { [postId: string]: IProps }
+	allIds: string[]
+}
+
+const posts: IProps[] = [
 	{
 		id: '62e69d5a5458aac0ed320b35',
 		title: 'id labore ex et quam laborum',
@@ -23,31 +33,19 @@ const posts = [
 		id: '62e69d5a5458aac0ed320b53',
 		title: 'vero eaque aliquid doloribus et culpa',
 		body: 'harum non quasi et rationetempore iure ex voluptates in rationeharum architecto fugit inventore cupiditatevoluptates magni quo et'
-	},
-	{
-		id: '62e69d5a5458aac0ed320b19',
-		title: 'et fugit eligendi deleniti quidem qui sint nihil autem',
-		body: 'doloribus at sed quis culpa deserunt consectetur qui praesentiumaccusamus fugiat dictavoluptatem rerum ut voluptate autemvoluptatem repellendus aspernatur dolorem in'
-	},
-	{
-		id: '62e69d5a5458aac0ed320b25',
-		title: 'repellat consequatur praesentium vel minus molestias voluptatum',
-		body: 'maiores sed dolores similique labore et inventore etquasi temporibus esse sunt id eteos voluptatem aliquamratione corporis molestiae mollitia quia et magnam dolor'
 	}
 ]
 
-const normalizeData = unnormalizedData => {
-	// Your code here...
+const normalizeData = (data: IProps[]): NormalizedData => {
+	const byId: { [key: string]: IProps } = {}
+	const allIds: string[] = []
+
+	data.map(post => {
+		byId[post.id] = post
+		allIds.push(post.id)
+	})
+
+	return { byId, allIds }
 }
 
 console.log(normalizeData(posts))
-/**
- * {
- *    byId: {
- *      62e69d5a5458aac0ed320b35: { id: '...', title: '...', body: '...' },
- *      62e69d5a5458aac0ed320b1c: { id: '...', title: '...', body: '...' },
- *      ...
- *    },
- *    allIds: ['62e69d5a5458aac0ed320b35', '62e69d5a5458aac0ed320b1c', ...]
- * }
- */
