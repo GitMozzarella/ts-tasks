7b32ab1f9bbf5287aed3758da2a16e3d83ec4084
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-01-22 02:56:40 +0600

    task3 done

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,38 @@
 const COMMENTS_URL = 'https://jsonplaceholder.typicode.com/comments'
 
-const getData = url => {
-	// Your code here...
+interface Comment {
+	postId?: number
+	id: number
+	name?: string
+	email: string
+	body?: string
 }
 
-getData(COMMENTS_URL).then(data => {
-	// Your code here...
-})
+const getData = async (url: string): Promise<Comment[]> => {
+	try {
+		const response = await fetch(url)
+		const data = (await response.json()) as Comment[]
 
-/**
- * ID: 1, Email: Eliseo...
- * ID: 2, Email: Jayne_Kuhic...
- * ID: 3, Email: Nikita...
- * ID: 4, Email: Lew...
- * ...
- */
+		if (!Array.isArray(data)) {
+			throw new Error('Error receiving data')
+		}
+
+		return data.map(comment => ({
+			id: comment.id,
+			email: comment.email
+		}))
+	} catch (error) {
+		console.error('Error fetching comments', error)
+		throw error
+	}
+}
+
+getData(COMMENTS_URL)
+	.then((data: Comment[]) => {
+		data.forEach(comment => {
+			console.log(`ID: ${comment.id} , Email : ${comment.email}`)
+		})
+	})
+	.catch((error: Error) => {
+		console.error(error)
+	})
